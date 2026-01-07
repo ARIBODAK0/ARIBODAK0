7be0b5212174036aec028fb15e8a0ac787db3db2
ARIBODAK0 <aribodak0@gmail.com>
2026-01-07 11:23:39 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-subprojects {
-    configurations.configureEach {
-        resolutionStrategy {
-            force("androidx.transition:transition:1.5.0")
-            force("androidx.viewpager2:viewpager2:1.1.0")
-        }
-    }
+plugins {
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
