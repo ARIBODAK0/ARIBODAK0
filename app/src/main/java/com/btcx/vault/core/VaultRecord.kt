224eebb74abba346e78cc24cf55804efda5c2a43
ARIBODAK0 <aribodak0@gmail.com>
2026-02-22 00:46:29 +0800

    Delete app directory

--- app/src/main/java/com/btcx/vault/core/VaultRecord.kt
+++
@@ -1,7 +0,0 @@
-package com.btcx.vault.core
-
-data class VaultRecord(
-    val cid: String,
-    val hmac: String,
-    val size: Long
-)
--- app/src/main/java/com/btcx/vault/crypto/SecureWipe.kt
+++
@@ -1,7 +0,0 @@
-package com.btcx.vault.crypto
-
-object SecureWipe {
-    fun wipe(bytes: ByteArray) {
-        for (i in bytes.indices) bytes[i] = 0
-    }
-}
